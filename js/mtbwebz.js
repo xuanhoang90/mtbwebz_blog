2c1347c0992b8613f42b077d245fbdd9c4232328
xuanhoang90 <nevergiveup.xuanhoang@gmail.com>
2016-08-01 16:43:22 +0700

    First commit

---
+++ js/mtbwebz.js
@@ -0,0 +1,8 @@
+$(function(){
+	/******************************************************
+	 *	Project: Mtbwebz
+	 *	Written by: Xuan Hoang
+	 *	29-07-2016
+	 *	nevergiveup.xuanhoang@gmail.com
+	 *****************************************************/
+})
